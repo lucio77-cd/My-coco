2506a4c25da7c2efce9dd970bd499ec89cfb1d58
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-05-14 13:46:49 -0300

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,35 @@
 const CACHE_NAME = "mycoco-cache-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/capture.html",
-  "/css/style.css",
-  "/js/app.js"
+  "./",
+  "./index.html",
+  "./capture.html",
+  "./css/style.css",
+  "./css/dashboard.css",
+  "./css/capture.css",
+  "./js/app.js",
+  "./js/db.js",
+  "./js/camera.js",
+  "./js/dashboard.js",
+  "./js/food.js",
+  "./js/hydration.js",
+  "./js/stool.js"
 ];
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener("install", event => {
 
-    event.waitUntil(
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 
-      caches.open(CACHE_NAME)
-        .then(cache => {
+});
 
-          return cache.addAll(
-            urlsToCache
-          );
+self.addEventListener("fetch", event => {
 
-        })
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 
-    );
-
-  }
-);
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches.match(event.request)
-        .then(response => {
-
-          return response ||
-                 fetch(event.request);
-
-        })
-
-    );
-
-  }
-);
+});
